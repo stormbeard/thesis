9e14c91fdb63faa361bf8134941ab6bafb86ad99
Tony Allen <tallen@nutanix.com>
2017-04-24 03:55:42 -0700

    More experiment raw data. This time with curator off and more granularity in samples

--- data/plot_adaptive_vs_random_disk_usage.py
+++ data/plot_adaptive_vs_random_disk_usage.py
@@ -10,11 +10,16 @@ matplotlib.rcParams.update({'font.size': 22})
 
 svmips = ["10.61.192.108", "10.61.192.109", "10.61.192.110"]
 odd_man_out = "10.61.192.110"
-random_selection_test = "random_selection_2"
-adaptive_selection_test = "adaptive_selection_6"
+#tests_to_plot = ["random_selection_3",
+#                 "adaptive_selection_7",
+#                 "adaptive_selection_8"]
 
-random_selection_map = {}
-adaptive_selection_map = {}
+tests_to_plot = ["adaptive_selection_9",
+                 "adaptive_selection_10",
+                 "random_selection_4"]
+
+# Map from test to map from svm ip to fullness timeseries
+selection_maps = {}
 
 def plot_from_map(svmip, selection_map):
     random_sz = len(selection_map[svmip])
@@ -23,51 +28,50 @@ def plot_from_map(svmip, selection_map):
     plt.plot(x, y)
 
 # Calculate pct usages.
-for i in svmips:
-    # Adaptive test data.
-    data = open(adaptive_selection_test + "/" + i + ".txt", 'r').readlines()
-    usage_pcts = (
-        map(lambda x: 100 * float(x.split(",")[1]) / float(x.split(",")[0]),
-            data))
-    adaptive_selection_map[i] = list(usage_pcts)
+for test in tests_to_plot:
+    selection_maps[test] = {}
+    for i in svmips:
+        # Adaptive test data.
+        data = open(test + "/" + i + "-tier_usage.txt",
+                    'r').readlines()
+        usage_pcts = (
+            map(lambda x: 100 * float(x.split(",")[1]) / float(x.split(",")[0]),
+                data))
+        selection_maps[test][i] = list(usage_pcts)
 
-    # Random test data.
-    data = open(random_selection_test + "/" + i + ".txt", 'r').readlines()
-    usage_pcts = (
-        map(lambda x: 100 * float(x.split(",")[1]) / float(x.split(",")[0]),
-            data))
-    random_selection_map[i] = list(usage_pcts)
-
-adaptive_avg_fullness_primary = []
-random_avg_fullness_primary = []
-for ii in range(len(adaptive_selection_map[odd_man_out])):
-    adaptive_avg_fullness_primary.append(
-        (adaptive_selection_map["10.61.192.108"][ii] +
-         adaptive_selection_map["10.61.192.109"][ii]) / 2.0)
-for ii in range(len(random_selection_map[odd_man_out])):
-    random_avg_fullness_primary.append(
-        (random_selection_map["10.61.192.108"][ii] +
-         random_selection_map["10.61.192.109"][ii]) / 2.0)
+# Get the disk fullness values for each node.
+fullness_primaries = {}
+for test in tests_to_plot:
+    minrange = min(map(lambda x: len(x), selection_maps[test].values()))
+    print "minrange: " + str(minrange)
+    fullness_primaries[test] = []
+    # Calculate the average non-oddman fullness.
+    for ii in range(minrange):
+        non_oddman_total = 0
+        for ip, fullness_series in selection_maps[test].iteritems():
+            if ip == odd_man_out:
+                continue
+            non_oddman_total += fullness_series[ii]
+        fullness_primaries[test].append(float(non_oddman_total) /
+                                        (len(svmips) - 1))
 
 # Calculate deviation from the average fullness of the workload nodes.
-a_dev = []
-r_dev = []
-for ii in range(len(adaptive_selection_map[odd_man_out])):
-    a_dev.append(abs(adaptive_selection_map[odd_man_out][ii] -
-                 adaptive_avg_fullness_primary[ii]))
-for ii in range(len(random_selection_map[odd_man_out])):
-    r_dev.append(abs(random_selection_map[odd_man_out][ii] -
-                 random_avg_fullness_primary[ii]))
+deviations = {}
+for test in tests_to_plot:
+    deviations[test] = []
+    minrange = min(map(lambda x: len(x), selection_maps[test].values()))
+    for ii in range(minrange):
+        deviations[test].append(abs(selection_maps[test][odd_man_out][ii] -
+                                fullness_primaries[test][ii]))
 
 legend = []
-legend.append("fitness selection")
-plt.plot(range(len(a_dev)), a_dev)
+for test in tests_to_plot:
+    legend.append(test)
+    # TODO: make this in real time units via some map
+    plt.plot(range(len(deviations[test])), deviations[test])
 
-legend.append("random selection")
-plt.plot(range(len(r_dev)), r_dev)
-plt.legend(legend, loc="bottom right")
-
+plt.legend(legend)
 plt.title("Idle Node Hot-tier Deviation")
-plt.xlabel("Time")
+plt.xlabel("Time (arbitrary units)")
 plt.ylabel("% Deviation")
 plt.show()
